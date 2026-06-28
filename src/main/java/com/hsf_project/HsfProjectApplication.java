1e888b1cd40f5a2d980fa976acbff9c1b7d150a8
phongNiro <phongpor1234@gmail.com>
2026-06-28 14:38:24 +0700

    setup project

---
+++ src/main/java/com/hsf_project/HsfProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hsf_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsfProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HsfProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hsf_project/HsfProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsf_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsfProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
