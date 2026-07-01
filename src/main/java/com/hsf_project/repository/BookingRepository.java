9f3ddeb1b1df59636bdb79ca50f962d048b1bee6
Le Chi Tam <contact.lechitam@gmail.com>
2026-07-01 14:28:03 +0700

    check

--- src/main/java/com/hsf_project/repository/BookingRepository.java
+++ src/main/java/com/hsf_project/repository/BookingRepository.java
@@ -4,6 +4,12 @@ import com.hsf_project.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
+
+    // Tìm danh sách đặt vé của User, sắp xếp theo ngày đặt mới nhất
+    // và bỏ qua các bản ghi đã bị xóa ẩn (isDeleted = false)
+    List<Booking> findByUserIdAndIsDeletedFalseOrderByBookingDateDesc(Long userId);
 }
