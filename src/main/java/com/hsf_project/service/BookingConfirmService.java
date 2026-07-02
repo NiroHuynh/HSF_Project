cc64f69c6f8c59dae3df64b3b0cbfde7eee98786
hoanghai100305-code <hoanghai100305@gmail.com>
2026-07-02 19:53:02 +0700

    feat: add filter and review for customer

--- src/main/java/com/hsf_project/service/BookingConfirmService.java
+++ src/main/java/com/hsf_project/service/BookingConfirmService.java
@@ -4,7 +4,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
-public interface BookingConfirmService {
+public interface    BookingConfirmService {
     String confirmBooking(Long showtimeId, List<String> seatCodes, Map<Long, Integer> comboQuantities,
                           Long paymentMethodId, Long promotionId,
                           BigDecimal discountAmount);
