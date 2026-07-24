4023cbc5c8440fe1584c8b7fab00a01a652d4415
hoanghai100305-code <hoanghai100305@gmail.com>
2026-07-24 11:37:56 +0700

    fix: add more countries, edit ageRating

--- src/main/java/com/hsf_project/entity/enums/AgeRating.java
+++ src/main/java/com/hsf_project/entity/enums/AgeRating.java
@@ -1,9 +1,19 @@
 package com.hsf_project.entity.enums;
 
 public enum AgeRating {
-    P,
-    K,
-    T13,
-    T16,
-    T18
+    P("P - Phim dành cho mọi lứa tuổi"),
+    K("K - Dưới 13 tuổi xem cùng cha mẹ hoặc người giám hộ"),
+    T13("T13 - Từ đủ 13 tuổi trở lên"),
+    T16("T16 - Từ đủ 16 tuổi trở lên"),
+    T18("T18 - Từ đủ 18 tuổi trở lên");
+
+    private final String description;
+
+    AgeRating(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
